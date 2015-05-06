f05f1f2e1ad2f53febd33eb9102e4127dea4f816
Danika <danikachrunik@gmail.com>
2015-05-06 16:50:41 -0700

    finished project

--- image_slider/script.js
+++ image_slider/script.js
@@ -1,3 +1,66 @@
-function Slider() {
-	$(".slider #1").show("fade", 500);
+sliderInt = 1;
+sliderNext = 2;
+
+$(document).ready(function(){
+	$('#1').fadeIn(300);
+	startSlider();
+});
+
+function startSlider(){
+	count = $('#slider>img').size();
+
+	loop = setInterval(function(){
+
+		if(sliderNext>=count){
+			sliderNext = 1;
+			sliderInt = 1;
+		}
+
+		$('#slider>img').fadeOut(300);
+		$('#'+ sliderNext).fadeIn(300);
+
+		sliderInt = sliderNext;
+		sliderNext = sliderNext + 1;
+
+	}, 3000)
 }
+
+function prev(){
+	newSlide = sliderInt -1;
+	showSlide(newSlide);
+}
+
+function next(){
+newSlide = sliderInt +1;
+	showSlide(newSlide);
+}
+
+function stopLoop(){
+	window.clearInterval(loop);
+}
+
+function showSlide(id){
+	stopLoop();
+	if(id>=count){
+		sliderNext = 1;
+		sliderInt = 1;
+	} else if (id<=1){
+		id = count;
+	}
+
+	$('#slider>img').fadeOut(300);
+	$('#'+ id).fadeIn(300);
+
+	sliderInt = id;
+	sliderNext = id + 1;
+	startSlider();
+}
+
+$("#slider>img").hover(
+	function(){
+		stopLoop();
+	},
+	function(){
+		startSlider();
+	}
+);
